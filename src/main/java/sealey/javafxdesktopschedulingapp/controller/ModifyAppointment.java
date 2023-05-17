7025863af6d2d0025285f6c304198e668251483a
Max Sealey <maxsealey@users.noreply.github.com>
2023-05-17 12:10:58 -0700

    Created scene controllers, connected to fxml files

---
+++ src/main/java/sealey/javafxdesktopschedulingapp/controller/ModifyAppointment.java
@@ -0,0 +1,14 @@
+package sealey.javafxdesktopschedulingapp.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ModifyAppointment implements Initializable
+{
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}

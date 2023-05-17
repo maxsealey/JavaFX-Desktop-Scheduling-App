db23c82b7d43ec3e0537634cf177598f5e1f2e82
Max Sealey <maxsealey@users.noreply.github.com>
2023-05-17 12:17:28 -0700

    Created all scene files, controllers, database connections; fixed error loading login page in Main

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module sealey.javafxdesktopschedulingapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     //opens sealey.javafxdesktopschedulingapp.model to javafx.base;
     opens sealey.javafxdesktopschedulingapp.util to javafx.base;
     exports sealey.javafxdesktopschedulingapp;
     opens sealey.javafxdesktopschedulingapp to javafx.base, javafx.fxml;
+    exports sealey.javafxdesktopschedulingapp.controller;
+    opens sealey.javafxdesktopschedulingapp.controller to javafx.base, javafx.fxml;
 }
